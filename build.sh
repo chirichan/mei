cc84372976820ea8f7f47fdc7693d1af394d5648
chirichan <57004799+chirichan@users.noreply.github.com>
2023-09-16 18:03:17 +0800

    Add release script

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+rm -f ./release/pwdgen-darwin-arm64
+rm -f ./release/pwdgen-darwin-x86_64
+rm -f ./release/pwdgen-linux-x86_64
+rm -f ./release/pwdgen-windows-x86_64.exe
+
+rm -f ./release/bing15-darwin-arm64
+rm -f ./release/bing15-darwin-x86_64
+rm -f ./release/bing15-linux-x86_64
+rm -f ./release/bing15-windows-x86_64.exe
+
+CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -o ./release/pwdgen-darwin-arm64 ./cmd/pwdgen/
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o ./release/pwdgen-darwin-x86_64 ./cmd/pwdgen/
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ./release/pwdgen-linux-x86_64 ./cmd/pwdgen/
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o ./release/pwdgen-windows-x86_64.exe ./cmd/pwdgen/
+
+CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -o ./release/bing15-darwin-arm64 ./cmd/bing15/
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o ./release/bing15-darwin-x86_64 ./cmd/bing15/
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ./release/bing15-linux-x86_64 ./cmd/bing15/
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o ./release/bing15-windows-x86_64.exe ./cmd/bing15/
