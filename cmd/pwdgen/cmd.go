364263971502831f4c48c4b648343b59db83b447
chirichan <57004799+chirichan@users.noreply.github.com>
2024-04-04 21:49:24 +0800

    Clear code

--- cmd/pwdgen/cmd.go
+++ cmd/pwdgen/cmd.go
@@ -1,17 +1,55 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"github.com/chirichan/mei/version"
 	"log"
+	"log/slog"
+	"net/url"
+	"os"
+	"path/filepath"
+	"time"
 
 	"github.com/atotto/clipboard"
+	"github.com/chirichan/mei/version"
+	"github.com/gocarina/gocsv"
 	"github.com/spf13/cobra"
 )
 
-type PwdgenCLI struct{}
+type XyKey struct {
+	Version int   `json:"version"`
+	Key     []Key `json:"key"`
+}
 
-func (m *PwdgenCLI) Root(cmd *cobra.Command, args []string) {
+type Key struct {
+	Name      string  `json:"name"`
+	Account   string  `json:"account"`
+	Password  string  `json:"password"`
+	Password2 string  `json:"password2"`
+	Url       string  `json:"url"`
+	Note      string  `json:"note"`
+	Extra     []Extra `json:"extra"`
+}
+
+type Extra struct {
+	Name    string `json:"name"`
+	Content string `json:"content"`
+}
+
+// ChromeCSV chrome, edge csv password
+type ChromeCSV struct {
+	Name     string `json:"name" csv:"name"`
+	URL      string `json:"url" csv:"url"`
+	Username string `json:"username" csv:"username"`
+	Password string `json:"password" csv:"password"`
+	Note     string `json:"note" csv:"note"`
+}
+
+type PwdGenCLI struct {
+	Logger *slog.Logger
+}
+
+func (m *PwdGenCLI) Root(cmd *cobra.Command, args []string) {
 	if v, _ := cmd.Flags().GetBool("version"); v {
 		fmt.Printf("pwdgen version is %s", version.Version)
 		return
@@ -34,8 +72,62 @@ func (m *PwdgenCLI) Root(cmd *cobra.Command, args []string) {
 	}
 }
 
+func (m *PwdGenCLI) Csv2Xykey(cmd *cobra.Command, args []string) error {
+	var csvData []ChromeCSV
+	if len(args) == 0 {
+		csvText, err := clipboard.ReadAll()
+		if err != nil {
+			return err
+		}
+		if err := gocsv.UnmarshalString(csvText, &csvData); err != nil {
+			return err
+		}
+	} else {
+		m.Logger.Info("csv2xykey", "args", args)
+		csvFile, err := os.OpenFile(args[0], os.O_RDONLY, 0)
+		if err != nil {
+			return err
+		}
+		defer csvFile.Close()
+		if err := gocsv.UnmarshalFile(csvFile, &csvData); err != nil {
+			return err
+		}
+	}
+	xykeyData := m.csv2Xykey(csvData)
+	xyKeyBytes, err := json.Marshal(xykeyData)
+	if err != nil {
+		return err
+	}
+	cwd := filepath.Dir(os.Args[0])
+	m.Logger.Info("save", "dir", cwd)
+	return os.WriteFile(fmt.Sprintf("xykey-%d.json", time.Now().Unix()), xyKeyBytes, 0644)
+}
+
+func (m *PwdGenCLI) csv2Xykey(csvData []ChromeCSV) *XyKey {
+	xyKey := &XyKey{
+		Version: 1,
+		Key:     make([]Key, len(csvData)),
+	}
+	for i, v := range csvData {
+		parse, err := url.Parse(v.URL)
+		if err != nil {
+			m.Logger.Error("parse csv name", "name", v.Name)
+		}
+		xyKey.Key[i] = Key{
+			Name:     parse.Hostname(),
+			Account:  v.Username,
+			Password: v.Password,
+			Url:      v.URL,
+			Note:     v.Note,
+			Extra:    make([]Extra, 0),
+		}
+	}
+	return xyKey
+}
+
 func NewCLI() *cobra.Command {
-	muCLI := &PwdgenCLI{}
+	muCLI := &PwdGenCLI{Logger: slog.Default()}
+
 	rootCmd := &cobra.Command{
 		Use:   "pwdgen",
 		Short: "生成随机密码",
@@ -46,5 +138,15 @@ func NewCLI() *cobra.Command {
 	rootCmd.Flags().IntP("level", "l", 4, "生成的密码强度等级, 数字越大, 强度越高, [1, 4]")
 	rootCmd.Flags().IntP("output", "o", 1, "输出方式, 1: 剪贴板, 2: 控制台")
 
+	csv2XykeyCmd := &cobra.Command{
+		Use:   "csv2xykey",
+		Short: "浏览器导出的 csv 格式的密码转为 xykey 格式。",
+		Args:  cobra.MaximumNArgs(1),
+		RunE:  muCLI.Csv2Xykey,
+	}
+
+	rootCmd.AddCommand(
+		csv2XykeyCmd,
+	)
 	return rootCmd
 }
